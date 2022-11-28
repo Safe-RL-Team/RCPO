43dcbb59f3d45b5b81e5fb6b7c2f07cbc2858430
sudo-Boris <boris.meinardus00@gmail.com>
2022-11-28 09:04:00 +0100

    Create poetry env and run script for sanity check.

---
+++ run.py
@@ -0,0 +1,26 @@
+import gym
+
+from stable_baselines3 import PPO
+
+### Create python env
+#   0. For mujoco envs, you need to install mujoco (mujoco-py already defined in pyproject.toml)
+#      https://github.com/openai/mujoco-py/
+#   1. poetry shell
+#   2. python setup.py install
+#   3. poetry install
+
+# env = gym.make("CartPole-v1")
+env = gym.make("HalfCheetah-v2")
+
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10_000)
+
+obs = env.reset()
+for _ in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+        obs = env.reset()
+
+env.close()
