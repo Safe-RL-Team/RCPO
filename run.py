90611e223bcd56182c942ba3b80267abdd7a8f9b
tuananhroman <anh.maxiking@hotmail.de>
2022-12-22 20:23:46 +0100

    wrappes main procedure in a function

--- run.py
+++ run.py
@@ -13,34 +13,39 @@ from stable_baselines3 import PPO, RCPPO
 #   4. cd gym-0.21.0 && pip install -e .
 
 
-### Example for cartpole env
-#   constraint is position of cartpole has to be to the left of the upper bound alpha
-env = gym.make("CartPole-v1")
+def main():
+    ### Example for cartpole env
+    #   constraint is position of cartpole has to be to the left of the upper bound alpha
+    env = gym.make("CartPole-v1")
 
-model = RCPPO(
-    "MlpPolicy",
-    env,
-    verbose=1,
-    constraint_alpha=-2,
-    lr_constraint_lambda_decay_threshold=0.3,
-    constant_constraint_lambda=None,
-    lr_constraint_lambda=0.02,
-    learning_rate=0.03,
-    tensorboard_log="/Users/borismeinardus/personal_projects/safeRL/stable-baselines3/tensorboard",
-)
+    model = RCPPO(
+        "MlpPolicy",
+        env,
+        verbose=1,
+        constraint_alpha=-2,
+        lr_constraint_lambda_decay_threshold=0.3,
+        constant_constraint_lambda=None,
+        lr_constraint_lambda=0.02,
+        learning_rate=0.03,
+        tensorboard_log="/Users/borismeinardus/personal_projects/safeRL/stable-baselines3/tensorboard",
+    )
 
-### Example for halfcheetah env
-# env = gym.make("HalfCheetah-v2")
-# model = RCPPO("MlpPolicy", env, verbose=1, constraint_alpha=1, lr_constraint_lambda=0.1)
+    ### Example for halfcheetah env
+    # env = gym.make("HalfCheetah-v2")
+    # model = RCPPO("MlpPolicy", env, verbose=1, constraint_alpha=1, lr_constraint_lambda=0.1)
 
-model.learn(total_timesteps=300_000)
+    model.learn(total_timesteps=300_000)
 
-obs = env.reset()
-for _ in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        obs = env.reset()
+    obs = env.reset()
+    for _ in range(1000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
 
-env.close()
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
