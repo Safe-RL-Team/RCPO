f93a1e848390e529c2114ee88b69de1b23aa95b3
sudo-Boris <boris.meinardus00@gmail.com>
2023-01-16 16:30:45 +0100

    Implement testing of trained model.

---
+++ test.py
@@ -0,0 +1,29 @@
+import torch
+import gym
+
+from stable_baselines3 import RCPPO
+
+
+def main():
+    print("evaluating model")
+
+    # Load pytorch model
+    model_name = "HalfCheetah-v3_100_16-01-2023_16-21-26"
+    model = RCPPO.load(f"runs/{model_name}")
+
+    # Get env of model
+    env = gym.make(model_name.split("_")[0])
+
+    obs = env.reset()
+    for _ in range(5000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
